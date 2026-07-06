bf1dabbc4f97d2c281e65c20e1331d4815cce47d
Karan Champaneri <k3champa@uwaterloo.ca>
2026-07-06 12:21:38 -0400

    part 1b exercise 1.3-1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,60 @@
 const App = () => {
 
-  const parts = ['Fundamentals of React', 'Using props to pass data', 'State of a component']
-  const exercises = [10, 7, 14]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
   return (
     <div>
-      <Header course = 'Half Stack application development' />
-      <Content parts = {parts} exercises = {exercises} />
-      <Total exercises = {exercises}></Total>
-
+      <Header course = {course} />
+      <Content course={course} />
+      <Total   course={course} />
     </div>
   )
 }
 
 const Header = ({course}) =>{
   return (
-      <h1>{course}</h1>
+      <h1>{course.name}</h1>
   )
 }
 
-const Part = ({part, exercise}) =>{
+const Part = ({part}) =>{
   return (
       <p>
-        {part} {exercise}
+        {/* render name and exercise from single object */}
+        {part.name} {part.exercises} 
       </p>
   )
 }
 
-const Content = ({parts, exercises}) => {
+const Content = ({course}) => {
   return(
     <div>
-      <Part part={parts[0]} exercise={exercises[0]} />
-      <Part part={parts[1]} exercise={exercises[1]} />
-      <Part part={parts[2]} exercise={exercises[2]} />
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
     </div>
   )
 }
 
-const Total = ({exercises}) =>{
+const Total = ({course}) =>{
   return (
-      <p>Number of exercises {exercises[0] + exercises[1] + exercises[2]} </p>
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} </p>
   )
 }
 
