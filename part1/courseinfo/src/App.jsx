b26df2662141c69fcb903ae4a4effd9c5eb17e19
Karan Champaneri <k3champa@uwaterloo.ca>
2026-07-07 10:56:40 -0400

    Experiment with React state and event handlers

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,61 +1,38 @@
-const App = () => {
+import {useState} from 'react'
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+const Display = (props) => {
   return (
-    <div>
-      <Header course = {course} />
-      <Content course={course} />
-      <Total   course={course} />
-    </div>
+    <div>{props.counter}</div>
   )
 }
 
-const Header = ({course}) =>{
-  return (
-      <h1>{course.name}</h1>
-  )
-}
-
-const Part = ({part}) =>{
-  return (
-      <p>
-        {/* render name and exercise from single object */}
-        {part.name} {part.exercises} 
-      </p>
-  )
-}
-
-const Content = ({course}) => {
+const Button = (props) => {
   return(
-    <div>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
-    </div>
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
   )
 }
 
-const Total = ({course}) =>{
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
-      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} </p>
+
+    <div>
+    <Display counter = {counter}/>
+    <Button onClick = {increaseByOne} text = 'plus'/>
+    <Button onClick = {decreaseByOne} text = 'minus'/>
+    <Button onClick={setToZero} text = 'reset'/>
+    </div>
+
   )
 }
+
+
 
 export default App
