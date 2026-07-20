7c38934087af2c296804772e102050029f7d0fa6
Karan Champaneri <k3champa@uwaterloo.ca>
2026-07-20 18:27:49 -0400

    part2b exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040 - 1234567" },
+  ]);
   const [newName, setNewName] = useState(""); // for form input element
+  const [newNum, setNewNum] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault(); //prevent screenrefresh
     const personObject = {
       //create new person object
       name: newName,
+      number: newNum,
     };
 
     // call function to check if personObject Exists in list.
@@ -19,6 +23,7 @@ const App = () => {
     }
     setPersons(persons.concat(personObject)); //add to list
     setNewName(""); //reset input field
+    setNewNum("");
   };
 
   const checkDuplicates = (newPerson) => {
@@ -33,6 +38,11 @@ const App = () => {
     setNewName(event.target.value); //update input state with each key input
   };
 
+  const handleNumChange = (event) => {
+    console.log(event.target.value);
+    setNewNum(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -41,13 +51,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNum} onChange={handleNumChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) => (
-          <div key={persons.indexOf(person)}>{person.name}</div>
+          <div key={persons.indexOf(person)}>
+            {person.name} - {person.number}
+          </div>
         ))}
       </div>
     </div>
